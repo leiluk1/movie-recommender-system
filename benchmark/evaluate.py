12938356619a6f3da82d83e940185b35f206aa1f
Leila Khaertdinova <ley.khaertdinova@mail.ru>
2023-12-02 22:15:51 +0300

    Add docstrings to code

--- benchmark/evaluate.py
+++ benchmark/evaluate.py
@@ -1,6 +1,20 @@
 from implicit.evaluation import precision_at_k, mean_average_precision_at_k, ndcg_at_k
 
+
 def evaluate(model, train_users_sparse, test_users_sparse):
+    """
+    Evaluate the performance of a movie recommendation model using Precision@k, MAP@k, 
+    and NDCG@k metrics for k=10.
+    
+    Parameters:
+    model: The movie recommendation model to be evaluated.
+    train_users_sparse: The sparse matrix with training user-item interactions.
+    test_users_sparse: The sparse matrix with test user-item interactions.
+    
+    Returns:
+    tuple: A tuple containing the Precision@10, mean average precision@10, and NDCG@10 scores.
+    """
+    
     p10 = precision_at_k(model, 
                          train_users_sparse, 
                          test_users_sparse, 
